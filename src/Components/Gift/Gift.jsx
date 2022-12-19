ae5052f42f1bd1cf9e01f4c0ea772bbeb29a540b
Manish <maknish1993@gmail.com>
2022-12-19 23:05:03 +0530

    Initial Commit

---
+++ src/Components/Gift/Gift.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Momgift, { Motherfaq, Therealdeal } from "./Momgift";
+import Shopyourway from "./Shopyourway";
+
+const Gift = () => {
+  return (
+    <>
+      <Momgift />
+      <Therealdeal />
+      <Motherfaq />
+      <Shopyourway />
+    </>
+     
+  
+  );
+};
+
+export default Gift;
